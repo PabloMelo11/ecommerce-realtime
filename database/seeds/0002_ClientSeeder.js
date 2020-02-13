683af44d4be91b05f640436b4bd83237f83c9077
Pablo Melo <pablomelodev@gmail.com>
2020-02-12 23:35:19 -0300

    Create role and client seeder

---
+++ database/seeds/0002_ClientSeeder.js
@@ -0,0 +1,37 @@
+/*
+|--------------------------------------------------------------------------
+| ClientSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory');
+const Role = use('Role');
+const User = use('App/Models/User');
+
+class ClientSeeder {
+  async run() {
+    const role = await Role.findBy('slug', 'client');
+    const clients = await Factory.model('App/Models/User').createMany(30);
+    await Promise.all(
+      clients.map(async client => {
+        await client.roles().attach([role.id]);
+      })
+    );
+
+    const user = await User.create({
+      name: 'Pablo',
+      surname: 'Melo',
+      email: 'pablo@pisystem.com.br',
+      password: 'secret',
+    });
+    await Role.findBy('slug', 'admin');
+    await user.roles().attach([role.id]);
+  }
+}
+
+module.exports = ClientSeeder;
