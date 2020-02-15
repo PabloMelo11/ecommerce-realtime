337de7b64abe3c936c8b1e473606ad1693380be3
Pablo Melo <pablomelodev@gmail.com>
2020-02-14 23:39:06 -0300

    Configuration of the routing

---
+++ app/Controllers/Http/Auth/AuthController.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+class AuthController {
+  async register({ request, response }) {
+    //
+  }
+
+  async login({ request, response, auth }) {
+    //
+  }
+
+  async refrash({ request, response, auth }) {
+    //
+  }
+
+  async logout({ request, response, auth }) {
+    //
+  }
+
+  async forgot({ request, response }) {
+    //
+  }
+
+  async remember({ request, response }) {
+    //
+  }
+
+  async reset({ request, response }) {
+    //
+  }
+}
+
+module.exports = AuthController;
---
+++ start/routes/auth.js
@@ -0,0 +1,14 @@
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route');
+
+/**
+ * Auth Routes
+ */
+Route.group(() => {
+  Route.post('/register', 'AuthController.register ').as('auth.register');
+  Route.post('/login', 'AuthController.login').as('auth.login');
+  Route.post('/refresh', 'AuthController.refrash').as('auth.refrash');
+  Route.post('/logout', 'AuthController.logout').as('auth.logout');
+})
+  .prefix('v1/auth')
+  .namespace('Auth');
