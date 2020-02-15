7b5431332a6b4bba4c2c272d07a73ff8df56f568
Pablo Melo <pablomelodev@gmail.com>
2020-02-15 00:18:04 -0300

    authentication routes

--- start/routes/auth.js
+++ start/routes/auth.js
@@ -5,10 +5,15 @@ const Route = use('Route');
  * Auth Routes
  */
 Route.group(() => {
-  Route.post('/register', 'AuthController.register ').as('auth.register');
-  Route.post('/login', 'AuthController.login').as('auth.login');
-  Route.post('/refresh', 'AuthController.refrash').as('auth.refrash');
-  Route.post('/logout', 'AuthController.logout').as('auth.logout');
+  Route.post('register', 'AuthController.register ').as('auth.register');
+  Route.post('login', 'AuthController.login').as('auth.login');
+  Route.post('refresh', 'AuthController.refrash').as('auth.refrash');
+  Route.post('logout', 'AuthController.logout').as('auth.logout');
+
+  // restore password routes
+  Route.post('reset-password', 'AuthController.forgot').as('auth.forgot');
+  Route.get('reset-password', 'AuthController.remember').as('auth.remember');
+  Route.put('reset-password', 'AuthController.reset').as('auth.reset');
 })
   .prefix('v1/auth')
   .namespace('Auth');
