2686f52b35e4ec14dfbc27458ecee271e6e59544
Pablo Melo <pablomelodev@gmail.com>
2020-02-18 23:55:47 -0300

    User registration

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,7 +1,33 @@
+const Database = use('Database');
+const User = use('App/Models/User');
+const Role = use('Role');
 /* eslint-disable no-unused-vars */
 class AuthController {
   async register({ request, response }) {
-    //
+    const trx = await Database.beginTransaction();
+    try {
+      const { name, surname, email, password } = request.all();
+
+      const user = await User.create(
+        {
+          name,
+          surname,
+          email,
+          password,
+        },
+        trx
+      );
+
+      const userRole = await Role.findBy('slug', 'client');
+      await user.role().attach([userRole.id], null, trx);
+      await trx.commit();
+      return response.status(201).json({ data: user });
+    } catch (error) {
+      await trx.rollback();
+      return response
+        .status(400)
+        .json({ error: 'Erro ao realizar o cadastro' });
+    }
   }
 
   async login({ request, response, auth }) {
